90224b5a5d53ab688cf3df7fe649719352d78635
Eugene Korenevsky <kara-91@tut.by>
2021-09-03 11:11:58 +0300

    add footer

--- src/components/footer/Index.tsx
+++ src/components/footer/Index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import './footer.css';
+import rsLogo from '../../assets/rs_school_js.svg';
+import FooterNav from './FooterNav';
 
 export default function Footer(): JSX.Element {
-  return <div>Footer</div>
+  return <div className="footer">
+    <div className="footer__first-line"></div>
+    <div className="footer__second-line">
+      <a className="footer__rs-link" href="https://rs.school/react/">
+        <img src={rsLogo} className="footer__rs-logo" alt="rsschool logo" />
+      </a>
+      <FooterNav/>
+      <div className="footer__year">2021</div>
+    </div>
+  </div>
 }
