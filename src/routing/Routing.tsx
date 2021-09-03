23a32283623a9735063634f56c07cd8f91f1882e
Eugene Korenevsky <kara-91@tut.by>
2021-09-03 08:12:01 +0300

    fix: fix game page path

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch, useLocation } from "react-router-dom";
 import MainPage from '../components/main_page/Index';
 import { NotFoundPage } from '../components/not_found/Index';
-import GamePage from '../components/wrapper_page/game_page/Index';
+import WrapperPage from '../components/wrapper_page/Index';
 
 export default function Routing(): JSX.Element {
   const location = useLocation();
@@ -10,7 +10,7 @@ export default function Routing(): JSX.Element {
   return <div>
     <Switch>
       <Route exact path="/" component={MainPage} />
-      <Route exact path="/game" component={GamePage} />
+      <Route exact path="/game/:id" component={WrapperPage} />
       <Route component={NotFoundPage} />
     </Switch>
   </div>;
