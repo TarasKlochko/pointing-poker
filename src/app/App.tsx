349de34f799c397e1c9a41481ee2f4af1e94c470
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-09-04 10:54:58 +0300

    feat: add fonts Roboto and Ruda

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import './fonts.css';
 import './App.css';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Routing from '../routing/Routing';
 
 function App(): JSX.Element {
-  return <div className="app">
-    <header>
-      <Header/>
-    </header>
-    <main className="app-main">
-      <HashRouter>
-        <Routing/>
-      </HashRouter>
-    </main>
-    <footer>
-      <Footer/>
-    </footer>
-  </div>;
+  return (
+    <div className="app">
+      <header>
+        <Header />
+      </header>
+      <main className="app-main">
+        <HashRouter>
+          <Routing />
+        </HashRouter>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
