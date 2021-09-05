f5f00a6a18f3f9cb4af549af742c97026163d488
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-09-05 17:51:14 +0300

    feat: add main-page

--- src/components/mainPage/index.tsx
+++ src/components/mainPage/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { ConnectPopup } from './connectPopup';
+import { Popup } from './popup';
 import './mainPage.css';
+import { isObsorverShow, isPopupAction } from './popup/popupSlice';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { createGameAction, IDGameAction } from './createGame.slice';
 
 export default function MainPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const isPopup = useAppSelector((state) => state.popup.isPopup);
+
+  function handleClickStart() {
+    dispatch(isPopupAction(true));
+    dispatch(isObsorverShow(false));
+    dispatch(createGameAction(true));
+  }
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    dispatch(IDGameAction(event.target.value));
+  }
+  function handleClickConnect() {
+    dispatch(createGameAction(false));
+    dispatch(isPopupAction(true));
+    dispatch(isObsorverShow(true));
+  }
+
   return (
     <section className="main-page">
       <h1 className="main-page__title">
@@ -12,7 +32,9 @@ export default function MainPage(): JSX.Element {
       <h2 className="main-page__subtitle main-page__subtitle_start">Start your planning:</h2>
       <div className="main-page__wrap">
         <p className="main-page__label">Create session:</p>
-        <button className="main-page__button">Start new game</button>
+        <button className="main-page__button" onClick={handleClickStart}>
+          Start new game
+        </button>
       </div>
 
       <h2 className="main-page__subtitle main-page__subtitle_or">OR:</h2>
@@ -20,10 +42,12 @@ export default function MainPage(): JSX.Element {
         Connect to lobby by <span className="main-page__label_id">ID</span>:
       </p>
       <div className="main-page__wrap">
-        <input className="main-page__input" type="numtextber" />
-        <button className="main-page__button">Connect</button>
+        <input className="main-page__input" type="numtextber" onChange={handleInput} />
+        <button className="main-page__button" onClick={handleClickConnect}>
+          Connect
+        </button>
       </div>
-      <ConnectPopup></ConnectPopup>
+      {isPopup && <Popup></Popup>}
     </section>
   );
 }
