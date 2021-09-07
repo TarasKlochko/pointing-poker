699ee89f7ba1f8f0d1b08cc1186fd714cf69322a
Eugene Korenevsky <kara-91@tut.by>
2021-09-07 11:01:35 +0300

    feat: add logic to lobby start-exit buttons

--- src/components/wrapperPage/lobby/scrumMasterBlock/index.tsx
+++ src/components/wrapperPage/lobby/scrumMasterBlock/index.tsx
@@ -10,7 +10,7 @@ import { UserRole } from '../../../../model/UserRole';
 
 const users: User[] = usersJSON
 
-const currentUser = users[1];
+const currentUser = users[0];
 
 export default function ScrumMasterBlock(): JSX.Element {
   const classes = useButtonStyles();
@@ -18,22 +18,35 @@ export default function ScrumMasterBlock(): JSX.Element {
   return <div className="scrum-master">
     <h5 className="scrum-master__title">Scram master:</h5>
     <MemberCard kind={MemberCardKind.SIMPLE} user={currentUser}></MemberCard>
-    <div className="scrum-master__label-block">
-      <h2 className="scrum-master__label-block__label">Label:</h2>
-      <div className="scrum-master__label-block__input-block">
-        <input type="text" className="scrum-master__label-block__input-block-input"></input>
-        <Button className={classes.blueButton} onClick={(): void => console.log('click')}
-          variant="contained" color="primary">Button</Button>
-      </div>
-    </div>
-    <div className="scrum-master__start-exit-buttons-wrapper">
-      {
-        currentUser.role === UserRole.DEALER? 
+    {
+      currentUser.role === UserRole.DEALER ? <div className="scrum-master__label-block">
+        <h2 className="scrum-master__label-block__label">Label:</h2>
+        <div className="scrum-master__label-block__input-block">
+          <input type="text" className="scrum-master__label-block__input-block-input"></input>
           <Button className={classes.blueButton} onClick={(): void => console.log('click')}
-            variant="contained" color="primary">Start Game</Button> : <div></div>
-      }
-      <Button className={`${classes.whiteButton} ${classes.marginButton}`} onClick={(): void => console.log('click')}
-        variant="contained" color="primary">Cancel Game</Button>
-    </div>
+            variant="contained" color="primary">Button</Button>
+        </div>
+      </div> : <div className="scrum-master__no-label-block"></div>
+    }
+
+    {
+      currentUser.role === UserRole.OBSERVER ? <div className="scrum-master__no-buttons"></div> :
+        <div className="scrum-master__start-exit-buttons-wrapper">
+          {
+            currentUser.role === UserRole.DEALER ?
+              <Button className={classes.blueButton} onClick={(): void => console.log('click')}
+                variant="contained" color="primary">Start Game</Button>
+              : <div></div>
+          }
+          {
+            currentUser.role === UserRole.DEALER ? <Button className={`${classes.whiteButton} ${classes.marginButton}`}
+              onClick={(): void => console.log('click')}
+              variant="contained" color="primary">Cancel Game</Button> :
+              <Button className={`${classes.whiteButton} ${classes.marginButton}`}
+                onClick={(): void => console.log('click')}
+                variant="contained" color="primary">Exit</Button>
+          }
+        </div>
+    }
   </div>
 }
