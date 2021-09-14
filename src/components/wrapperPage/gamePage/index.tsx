2d9a2576b17c59b49813ad8ddb95f1774e7e413c
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-09-14 19:32:36 +0300

    feat: add scram master view game (UI)

--- src/components/wrapperPage/gamePage/index.tsx
+++ src/components/wrapperPage/gamePage/index.tsx
@@ -1,5 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAppSelector } from '../../../app/hooks';
+import { MemberCardKind } from '../../../model/MemberCardKind';
+import GameCard from '../../common/gameCard';
+import MemberCard from '../../common/memberCard';
+import CardIssue from './cardIssue';
+import './gamePage.css';
 
 export default function GamePage(): JSX.Element {
-  return <div>Game page</div>
+  const game = useAppSelector((state) => state.game);
+  const [isRunRound, setIsRunRound] = useState(true);
+  const [isTimer, setIsTimer] = useState(true);
+  return (
+    <section className="game">
+      <div className="game__main main">
+        <h2 className="main__title">Game issues</h2>
+        <div className="main__top top">
+          <div className="top__master-wrap">
+            <h3 className="top__master-title">Scrum master:</h3>
+            <MemberCard user={game.dealer} kind={MemberCardKind.SIMPLE} />
+          </div>
+          <button className="top__button">Stop Game</button>
+        </div>
+        <div className="main__issues issues">
+          <h2 className="issuses__title">Issues:</h2>
+          <div className="issues__wrap">
+            <div className="issues__card-wrap">
+              {<CardIssue issue={'Issue 123'} priority={'Low priority'} current={false} link={'link'} score={'-'} />}
+              {<CardIssue issue={'Issue 124'} priority={'Low priority'} current={true} link={'link'} score={'-'} />}
+              {<CardIssue issue={'Issue 125'} priority={'Low priority'} current={false} link={'link'} score={'15'} />}
+            </div>
+            {isTimer && (
+              <div className="issues__control-wrap">
+                <div className="issues__control-timer"></div>
+                <div className="issues__control-buttons-wrap">
+                  {!isRunRound && <div className="issues__control-button">Run Round</div>}
+                  {isRunRound && (
+                    <>
+                      <div className="issues__control-button">Restart Round</div>
+                      <div className="issues__control-button">Next ISSUE</div>
+                    </>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="main__statistic statistic">
+          <h2 className="statistic__title">Statistics:</h2>
+          <div className="statistic__wrap">
+            <div className="statistic__card-wrap">
+              <GameCard value={'15'} scopeTypeShort={'SP'} statistic={true} />
+              <div className="statistic__score">15.3%</div>
+            </div>
+            <div className="statistic__card-wrap">
+              <GameCard value={'WWWW'} scopeTypeShort={'SP'} statistic={true} />
+              <div className="statistic__score">15.3%</div>
+            </div>
+            <div className="statistic__card-wrap">
+              <GameCard value={'15'} scopeTypeShort={'SP'} statistic={true} />
+              <div className="statistic__score">15.3%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="game__score"></div>
+    </section>
+  );
 }
