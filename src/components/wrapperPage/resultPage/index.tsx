ec6a0665096fc0ef89e8318fddd4588beb01f8a2
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-09-21 15:50:18 +0300

    feat: add the ability to save the result

--- src/components/wrapperPage/resultPage/index.tsx
+++ src/components/wrapperPage/resultPage/index.tsx
@@ -1,12 +1,94 @@
 import React from 'react';
 import { useAppSelector } from '../../../app/hooks';
+import ExportToExcel from './exportToExcel';
 import GameCard from '../../common/gameCard';
 import CardIssue from '../gamePage/cardIssue';
 import './resultPage.css';
 
+export type ResultInfo = {
+  column1: string;
+  column2: string;
+  column3?: string;
+  column4?: string;
+};
+
 export default function ResultPage(): JSX.Element {
-  const issues = useAppSelector((state) => state.game.room.issues);
+  const mockIssues = [
+    {
+      id: '',
+      priority: 'Low priority',
+      name: 'my issue1',
+      link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/pointing-poker.md',
+      score: '50',
+      statistic: [
+        {
+          value: '10',
+          percentage: ' 15%',
+        },
+        {
+          value: '30',
+          percentage: ' 25%',
+        },
+        {
+          value: '45',
+          percentage: ' 60%',
+        },
+      ],
+    },
+    {
+      id: '',
+      priority: 'Hight priority',
+      name: 'my issue2',
+      link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/pointing-poker.md',
+      score: '60',
+      statistic: [
+        {
+          value: '20',
+          percentage: ' 25%',
+        },
+        {
+          value: '40',
+          percentage: ' 35%',
+        },
+        {
+          value: '55',
+          percentage: ' 70%',
+        },
+      ],
+    },
+    {
+      id: '',
+      priority: 'Middle priority',
+      name: 'my issue3',
+      link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/pointing-poker.md',
+      score: '-',
+      statistic: [],
+    },
+  ];
   const gameSettings = useAppSelector((state) => state.gameSettings);
+  const fileName = 'game-result';
+  const issues = useAppSelector((state) => state.game.room.issues);
+  const resultReal: ResultInfo[] = [];
+
+  mockIssues.forEach((issue) => {
+    resultReal.push(
+      { column1: 'Issue', column2: issue.name },
+      { column1: 'Priority', column2: issue.priority },
+      { column1: 'Score', column2: issue.score || '-' },
+      {
+        column1: 'Link',
+        column2: issue.link,
+      },
+    );
+    if (issue.statistic.length) {
+      resultReal.push({ column1: 'Cards:', column2: '№', column3: 'Value', column4: 'Percentage' });
+      issue.statistic?.forEach((obj, index) => {
+        resultReal.push({ column1: '', column2: (index + 1).toString(), column3: obj.value, column4: obj.percentage });
+      });
+    }
+
+    resultReal.push({ column1: '', column2: '' });
+  });
 
   return (
     <section className="result">
@@ -31,6 +113,7 @@ export default function ResultPage(): JSX.Element {
           </li>
         ))}
       </ul>
+      <ExportToExcel data={resultReal} fileName={fileName} />
     </section>
   );
 }
