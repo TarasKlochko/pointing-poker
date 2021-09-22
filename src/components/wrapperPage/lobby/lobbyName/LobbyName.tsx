5bb0199726b0f6882274ed72340e708d1f54bb52
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-09-22 11:11:18 +0300

    feat: add changing room name for members before start game

--- src/components/wrapperPage/lobby/lobbyName/LobbyName.tsx
+++ src/components/wrapperPage/lobby/lobbyName/LobbyName.tsx
@@ -5,30 +5,33 @@ import './lobby-name.css';
 import { UserRole } from '../../../../model/UserRole';
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { setName } from '../../../../slices/GameSlice';
+import { Room } from '../../../../model/Room';
+import { Controller } from '../../../../api/Controller';
 
 export default function LobbyName(): JSX.Element {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
   const inputEl = useRef<HTMLInputElement>(null);
   const name = useAppSelector((state) => state.game.room.name);
+  const room = useAppSelector((state) => state.game.room);
+  const socket = useAppSelector((state) => state.socket.socket);
 
   useLayoutEffect(() => {
     const handleFocus = (): void => {
-      inputEl.current?.classList.add("active")
-    }
+      inputEl.current?.classList.add('active');
+    };
 
     const handleBlur = (): void => {
-      inputEl.current?.classList.remove("active")
-    }
-    inputEl.current?.addEventListener("focus", handleFocus)
-    inputEl.current?.addEventListener("blur", handleBlur)
+      inputEl.current?.classList.remove('active');
+    };
+    inputEl.current?.addEventListener('focus', handleFocus);
+    inputEl.current?.addEventListener('blur', handleBlur);
 
     return () => {
-      inputEl.current?.removeEventListener("focus", handleFocus)
-      inputEl.current?.removeEventListener("blur", handleBlur)
-    }
-
-  }, [])
+      inputEl.current?.removeEventListener('focus', handleFocus);
+      inputEl.current?.removeEventListener('blur', handleBlur);
+    };
+  }, []);
 
   const onButtonClick = () => {
     if (inputEl && inputEl.current) {
@@ -39,21 +42,34 @@ export default function LobbyName(): JSX.Element {
   const onChangeHandler = (e: React.FormEvent) => {
     const input = (e.target as HTMLInputElement).value;
     dispatch(setName(input));
-  }
+  };
 
-  const dealerEl = <div className="lobby-name">
-    <input value={name} ref={inputEl} type="text"
-      onChange={onChangeHandler}
-      className={`lobby-name__name`}></input>
-    <IconButton onClick={onButtonClick}>
-      <EditIcon />
-    </IconButton>
-  </div>
+  const handleInputOnBlur = () => {
+    Controller.updateRoom(socket, room);
+  };
 
-  const othersEl = <div className="lobby-name">
-    <h3 className="lobby-name__name lobby-name__others">{name}</h3>
-  </div>
+  const dealerEl = (
+    <div className="lobby-name">
+      <input
+        value={name}
+        ref={inputEl}
+        type="text"
+        onChange={onChangeHandler}
+        onBlur={handleInputOnBlur}
+        className={`lobby-name__name`}
+      ></input>
+      <IconButton onClick={onButtonClick}>
+        <EditIcon />
+      </IconButton>
+    </div>
+  );
 
-  const resEl = user.user.role === UserRole.DEALER? dealerEl : othersEl
-  return resEl
+  const othersEl = (
+    <div className="lobby-name">
+      <h3 className="lobby-name__name lobby-name__others">{name}</h3>
+    </div>
+  );
+
+  const resEl = user.user.role === UserRole.DEALER ? dealerEl : othersEl;
+  return resEl;
 }
