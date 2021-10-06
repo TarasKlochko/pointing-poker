c137d80d6d6e94c302ca9929f0b8cac02c279283
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-10-06 13:37:42 +0300

    fix: fix displaying observer in score/players after flip card

--- src/utils/VoteUtil.ts
+++ src/utils/VoteUtil.ts
@@ -1,7 +1,7 @@
-import { GameSettings } from "../components/wrapperPage/lobby/settingsBlock/settingBlog.slice";
-import { MemberVoteStatus, MemberVoteTicket } from "../model/MemberVote";
-import { User } from "../model/User";
-import { UserRole } from "../model/UserRole";
+import { GameSettings } from '../components/wrapperPage/lobby/settingsBlock/settingBlog.slice';
+import { MemberVoteStatus, MemberVoteTicket } from '../model/MemberVote';
+import { User } from '../model/User';
+import { UserRole } from '../model/UserRole';
 
 export interface MemberVoteResult {
   user: User;
@@ -9,25 +9,32 @@ export interface MemberVoteResult {
 }
 
 export class VoteUtil {
-  public static getVoteResult(members: User[], scopeTipeShort: string,
-    status: MemberVoteStatus, settings: GameSettings,
-    voteTickets?: MemberVoteTicket[]): MemberVoteResult[] {
-    const result: MemberVoteResult[] = [];  
+  public static getVoteResult(
+    members: User[],
+    scopeTipeShort: string,
+    status: MemberVoteStatus,
+    settings: GameSettings,
+    voteTickets?: MemberVoteTicket[],
+  ): MemberVoteResult[] {
+    const result: MemberVoteResult[] = [];
     if (status === MemberVoteStatus.FINISHED && voteTickets) {
-      members.forEach((userItem) => {
-        const res = voteTickets.filter((item) => userItem.id === item.userId);
-        if(res.length > 0) {
-          result.push({user: userItem, value: `${res[0].value} ${scopeTipeShort}`});
-        } else {
-          result.push({user: userItem, value: 'unknown'});
-        }
-      });
+      members
+        .filter((member) => member.role !== 'observer')
+        .forEach((userItem) => {
+          const res = voteTickets.filter((item) => userItem.id === item.userId);
+          if (res.length > 0) {
+            result.push({ user: userItem, value: `${res[0].value} ${scopeTipeShort}` });
+          } else {
+            result.push({ user: userItem, value: 'unknown' });
+          }
+        });
     } else {
       members.forEach((userItem) => {
-        if(userItem.role === UserRole.PLAYER) result.push({user: userItem, value: 'In progress'}); 
-        if(userItem.role === UserRole.DEALER && settings.isMasterAsPlayer) result.push({user: userItem, value: 'In progress'});
+        if (userItem.role === UserRole.PLAYER) result.push({ user: userItem, value: 'In progress' });
+        if (userItem.role === UserRole.DEALER && settings.isMasterAsPlayer)
+          result.push({ user: userItem, value: 'In progress' });
       });
     }
-    return result
+    return result;
   }
 }
